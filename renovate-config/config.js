74ee2f81dda2339d41382ea6d26653f8edc95dcc
Ansgar Hoyer <ansgar.hoyer@aoe.com>
2025-08-19 15:52:57 +0200

    feat: enable self hosted renovate

---
+++ renovate-config/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    platform: 'github',
+    autodiscover: true,
+    // autodiscoverFilter: ['.*'],
+    onboarding: true,
+    onboardingConfig: {
+        "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+        "extends": [
+            "github>konfidence-project/.github//renovate-config/presets/base.json5"
+        ]
+    },
+    hostRules: [
+        // GitHub API repo-scoped (für z.B. changelogs & lookups)
+        {
+            matchHost: 'https://api.github.com/repos/konfidence-project',
+            hostType: 'github',
+            token: process.env.RENOVATE_TOKEN,
+        },
+        // Raw git fetches (vendir git, git-refs, etc.)
+        {
+            matchHost: 'https://github.com/konfidence-project',
+            token: process.env.RENOVATE_TOKEN,
+        },
+    ]
+};
